251ae7b07dc1fae21a97c47335bf1b9ac828a036
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-05-11 12:47:57 +0900

    make it better

--- drfapi/settings.py
+++ drfapi/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
+    "http://localhost:3000","https://react-basic-drf210506.vercel.app"
 ]
 
 ROOT_URLCONF = 'drfapi.urls'
